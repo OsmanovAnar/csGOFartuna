2d6b4c2912ae5bf50eaf3fc30b09eeff4ecf973f
Anar Osmanov <anar_osmanov@vk.com>
2025-06-13 14:37:37 +0400

    Доработка функционала

--- script/script.js
+++ script/script.js
@@ -1,7 +1,11 @@
 const app = document.querySelector('.app');
-const allCardsEl = document.querySelector('.cards');
-const cardsEl = document.querySelector('.card');
-const spinBtn = document.querySelector('.spin-btn');
+const cardsContainer = document.querySelector('.cards');
+const cards = document.querySelectorAll('.card');
+const spinBtn = document.querySelector('#spin-btn');
+const closeBtn = document.querySelector('#close-btn')
+const dialog = document.querySelector('.wheel_result');
+const dialogImg = document.querySelector('.wheel_result-item img');
+const dialogName = document.querySelector('.wheel_result-item h3')
 
 const spinSound = new Audio('../sounds/spin.mp3')
 const winSound = new Audio('../sounds/win.mp3')
@@ -14,65 +18,55 @@ function spinKarusel() {
     requestAnimationFrame(() => {
         spinSound.play()
 
-        const index = Math.floor(Math.random() * cardsEl, length)
+        const index = Math.floor(Math.random() * cards.length)
         const angle = 6 * 360 - index * 60
 
+        console.log(index);
 
-        cardsEl.classList.add('spin')
-        cardsEl.style.transform = `rotateY(${angle}deg`
+
+
+        cardsContainer.classList.add('spin')
+        cardsContainer.style.transform = `rotateY(${angle}deg`
 
         setTimeout(() => {
-            cardsEl[index].classList.add('spin')
+            cards[index].classList.add('spin')
             spinBtn.disabled = false
-            showInModal(cardsEl[index])
+            shouWinModal(cards[index])
         }, 8000);
-
-
-
-
-
-
     })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
 
 
 
 
+function removeProps() {
+
+    spinBtn.disabled = true
+    cardsContainer.classList.remove('spin')
+    cardsContainer.style.transform = 'rotateY(0deg)'
+}
+
+//функция для показа рещультатов в модальном окне
+function shouWinModal(el) {
+    dialogImg.src = el.querySelector('img').src
+    dialogName.textContent = el.querySelector('.gun-name').textContent
+    winSound.play()
+    dialog.classList.add('active')
+}
 
 
+function init() {
+    spinBtn.addEventListener('click', spinKarusel)
+    closeBtn.addEventListener('click', () => dialog.classList.remove('active'))
+    document.addEventListener('keydown', e => {
+        if ((e.key === 'Enter' || e.code === 'Space') && !dialog.classList.contains('active')) {
+            spinKarusel()
+        }
 
+        if ((e.key === 'Escape' || e.key === 'Enter' || e.code === 'Space') && dialog.classList.contains('active')) {
+            dialog.classList.remove('active')
+        }
+    })
+}
 
-
-
-
-
-spinBtn.addEventListener('click', () => {
-
-})
+init();
